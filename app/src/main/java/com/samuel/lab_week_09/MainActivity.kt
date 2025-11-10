7ccd16c5390477ff5affb1e5d686ce57d749a451
MBotakk <samuel.itachis4@gmail.com>
2025-11-10 22:33:59 +0700

    Commit No. 2 – State and Event Handler

--- app/src/main/java/com/samuel/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/samuel/lab_week_09/MainActivity.kt
@@ -15,6 +15,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -33,77 +37,95 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val list = listOf("Tanu", "Tina", "Tono")
-                    Home(list)
+                    Home()
                 }
             }
         }
     }
 }
 
+data class Student(var name: String)
+
 @Composable
-fun Home(items: List<String>) {
-    //Here, we use LazyColumn to lazily display a list of items horizontally
-    //because it only composes and lays out the currently visible items
-    //You can also use LazyRow to display a list of items horizontally
+fun Home() {
+    //Here, we create a mutable state list of Student
+    //We use remember to make the list remember its value
+    //This is so that the list won’t be recreated when the composable
+    //recomposes
+    //We use mutableStateListOf to make the list mutable
+    //This is so that we can add or remove items from the List
+    //If you’re still confused, this is basically the same concept as
+    //using
+    //useState in React
+    val listData = remember {
+        mutableStateListOf(
+            Student("Tanu"),
+            Student("Tina"),
+            Student("Tono")
+        )
+    }
+    //Here, we create a mutable state of Student
+    //This is so that we can get the value of the input field
+    val inputField = remember { mutableStateOf(Student("")) }
+
+    //We call the HomeContent composable
+    //Here, we pass:
+    //listData to show the list of items inside HomeContent
+    //inputField to show the input field value inside HomeContent
+    //A lambda function to update the value of the inputField
+    //A lambda function to add the inputField to the listData
+    HomeContent(
+        listData = listData,
+        inputField = inputField.value,
+        onInputValueChange = { input -> inputField.value = inputField.value.copy(name = input) },
+        onButtonClick = {
+            if (inputField.value.name.isNotBlank()) {
+                listData.add(inputField.value)
+                inputField.value = Student("")
+            }
+        }
+    )
+}
+
+@Composable
+fun HomeContent(
+    listData: SnapshotStateList<Student>,
+    inputField: Student,
+    onInputValueChange: (String) -> Unit,
+    onButtonClick: () -> Unit
+) {
     LazyColumn {
         item {
             Column(
-                //Modifier.padding(16.dp) is used to add padding to the
-                //You can also use Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-                //to add padding horizontally and vertically
-                //or Modifier.padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
-                //to add padding to each side
                 modifier = Modifier.padding(16.dp).fillMaxSize(),
-                //Alignment.CenterHorizontally is used to align the Column
-                //horizontally
-                //You can also use verticalArrangement = Arrangement.Center
-                //to align the Column vertically
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(text = stringResource(id = R.string.enter_item))
-                //Here, we use TextField to display a text input field
                 TextField(
-                    //Set the value of the input field
-                    value = "",
-                    //Set the keyboard type of the input field
+                    value = inputField.name,
+                    onValueChange = onInputValueChange,
                     keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number
-                    ),
-                    //Set what happens when the value of the input field
-                    //changes
-                    onValueChange = {}
+                        keyboardType = KeyboardType.Text
+                    )
                 )
-                //Here, we use Button to display a button
-                //the onClick parameter is used to set what happens when the
-                //button is clicked
-                Button(onClick = {}) {
-                    //Set the text of the button
+                Button(onClick = onButtonClick) {
                     Text(text = stringResource(id = R.string.button_click))
                 }
             }
         }
-        //Here, we use items to display a list of items inside the
-        //LazyColumn
-        //This is the RecyclerView replacement
-        items(items) { item ->
+        items(listData) { item ->
             Column(
                 modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = item)
+                Text(text = item.name)
             }
         }
     }
 }
 
-
-//Here, we create a preview function of the Home composable
-//This function is specifically used to show a preview of the Home
-//composable
-//This is only for development purpose
 @Preview(showBackground = true)
 @Composable
 fun PreviewHome() {
-    Home(listOf("Tanu", "Tina", "Tono"))
+    Home()
 }
